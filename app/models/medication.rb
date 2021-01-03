69366671b70095ba22fddc33b7ceb6a0a97d1e15
jennifer-yoo <jennifer.yoo7@gmail.com>
2021-01-02 22:38:14 -0500

    relationships and seed data fixed

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,3 +1,3 @@
 class Medication < ApplicationRecord
-    belongs_to :order
+    has_many :orders
 end
--- app/models/supply.rb
+++ app/models/supply.rb
@@ -1,3 +1,3 @@
 class Supply < ApplicationRecord
-    belongs_to :order
+    has_many :orders
 end
